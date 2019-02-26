43dbe6e96cb8cf31b82d37812cd3984fd04605b7
Vishwanath Gangam <anjaiahspr@gmail.com>
2019-02-26 15:01:46 +0530

    'added'

--- python_codes/welcome.py
+++ python_codes/welcome.py
@@ -1,1 +1,1 @@
-print("File System -->Spark SQL ---> Cassardra --->Spark ML --->Train on this data Model --yyyyyyyyyyyyyyyyyyyyyyyyyy-> File System --->Spark Streaming --->>  Getting data from kafkajjjj")
+print("File System 000000000000000000-->Spark SQL ---> Cassardra --->Spark ML --->Train on this data Model --yyyyyyyyyyyyyyyyyyyyyyyyyy-> File System --->Spark Streaming --->>  Getting data from kafkajjjj")
